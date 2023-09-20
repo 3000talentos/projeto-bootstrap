6e2bb971cd4fae47b5c67e34fde02be7e864f534
3000talentos <130005833+3000talentos@users.noreply.github.com>
2023-09-19 21:00:13 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+
+
+const botao1 = `<a href="index.html" class="nav-link px-2 link-secondary">Home</a>`
+document.getElementById("botao1").innerHTML = botao1
+
+const botao2 = `<a href="teste.html" class="nav-link px-2 link-secondary">Teste</a>`
+document.getElementById("botao2").innerHTML = botao2
+
+const botao3 = `<a href="pagina.html" class="nav-link px-2 link-secondary">Página</a>`
+document.getElementById("botao3").innerHTML = botao3
+
